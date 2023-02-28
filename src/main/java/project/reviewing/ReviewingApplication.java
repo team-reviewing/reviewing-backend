d8e844ca162cc68661c0295565f54dcf5e5e76b2
ohseil <sioh0612@gmail.com>
2023-02-28 14:23:46 +0900

    feat: 프로젝트 초기 환경 구성

---
+++ src/main/java/project/reviewing/ReviewingApplication.java
@@ -0,0 +1,13 @@
+package project.reviewing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/reviewing/ReviewingApplicationTests.java
@@ -0,0 +1,13 @@
+package project.reviewing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
