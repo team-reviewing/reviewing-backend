849396543939f2498de2e41f6a062961f67e8754
SeungCheol <ssc6839@gmail.com>
2023-03-09 02:17:59 +0900

    feat: 경력 enum 클래스 추가

---
+++ src/main/java/project/reviewing/member/command/domain/Career.java
@@ -0,0 +1,34 @@
+package project.reviewing.member.command.domain;
+
+import java.util.Arrays;
+import lombok.Getter;
+import project.reviewing.member.exception.CareerNotFoundException;
+
+@Getter
+public enum Career {
+
+    NEWCOMER("신입", "(1년 이하)"),
+    JUNIOR("주니어", "(1 ~ 3년)"),
+    MIDDLE("미들", "(4 ~ 8년)"),
+    SENIOR("시니어", "(9년 이상)"),
+    ;
+
+    private final String value;
+    private final String year;
+
+    Career(final String value, final String year) {
+        this.value = value;
+        this.year = year;
+    }
+
+    public static Career findValue(final String value) {
+        return Arrays.stream(values())
+                .filter(career -> career.value.equals(value))
+                .findFirst()
+                .orElseThrow(CareerNotFoundException::new);
+    }
+
+    public String getCareer() {
+        return value + year;
+    }
+}
