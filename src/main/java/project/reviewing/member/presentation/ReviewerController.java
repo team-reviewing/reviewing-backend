2a61b0210ae337d3ba720993b0cdbebfa0bdcd4f
SeungCheol <ssc6839@gmail.com>
2023-03-15 14:32:08 +0900

    feat: page 파라미터에 올바르지 않은 값이 입력되는 경우 400을 반환하도록 구현

--- src/main/java/project/reviewing/member/presentation/ReviewerController.java
+++ src/main/java/project/reviewing/member/presentation/ReviewerController.java
@@ -1,17 +1,23 @@
 package project.reviewing.member.presentation;
 
+import javax.validation.constraints.PositiveOrZero;
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Validated
 @RequestMapping("/reviewers")
 @RequiredArgsConstructor
 @RestController
 public class ReviewerController {
 
     @GetMapping
-    public void findReviewers() {
+    public void findReviewers(
+            @PositiveOrZero @RequestParam final Long page
+    ) {
 
     }
 }
--- src/test/java/project/reviewing/unit/member/presentation/ReviewerControllerTest.java
+++ src/test/java/project/reviewing/unit/member/presentation/ReviewerControllerTest.java
@@ -7,6 +7,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 import project.reviewing.unit.ControllerTest;
 
 @DisplayName("ReviewerController 는")
@@ -19,9 +22,21 @@ public class ReviewerControllerTest extends ControllerTest {
         @DisplayName("정상적인 경우 200을 반환한다.")
         @Test
         void findReviewers() throws Exception {
-            mockMvc.perform(get("/reviewers"))
+            mockMvc.perform(get("/reviewers")
+                            .param("page", "1"))
                     .andDo(print())
                     .andExpect(status().isOk());
+        }
+
+        @DisplayName("page 파라미터에 올바르지 않은 값이 입력되는 경우 400을 반환한다.")
+        @ValueSource(strings = "-1")
+        @NullAndEmptySource
+        @ParameterizedTest
+        void findReviewer(final String page) throws Exception {
+            mockMvc.perform(get("/reviewers")
+                            .param("page", page))
+                    .andDo(print())
+                    .andExpect(status().isBadRequest());
         }
     }
 }
