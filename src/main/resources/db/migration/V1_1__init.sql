280824287d452648ca938575ed73a8ae1e5ad2bf
SeungCheol <ssc6839@gmail.com>
2023-03-18 20:00:56 +0900

    chore: InnoDB 엔진 설정

--- src/main/resources/db/migration/V1_1__init.sql
+++ src/main/resources/db/migration/V1_1__init.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS member (
     profile_url     VARCHAR(255)    NOT NULL    UNIQUE,
     is_reviewer     BOOLEAN         DEFAULT 0,
     PRIMARY KEY (id)
-);
+) ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS reviewer (
     id              BIGINT          NOT NULL    AUTO_INCREMENT,
@@ -17,20 +17,20 @@ CREATE TABLE IF NOT EXISTS reviewer (
     introduction    VARCHAR(255),
     PRIMARY KEY (id),
     FOREIGN KEY (member_id) REFERENCES member (id)
-);
+) ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS refresh_token (
     member_id       BIGINT          PRIMARY KEY,
     token           VARCHAR(255)    NOT NULL    UNIQUE,
     issued_at       DATETIME        NOT NULL,
     FOREIGN KEY (member_id) REFERENCES member (id)
-);
+) ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS category (
     id              BIGINT          NOT NULL    AUTO_INCREMENT,
     name            VARCHAR(255)    NOT NULL,
     PRIMARY KEY (id)
-);
+) ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS tag (
     id              BIGINT          NOT NULL    AUTO_INCREMENT,
@@ -38,7 +38,7 @@ CREATE TABLE IF NOT EXISTS tag (
     name            VARCHAR(255)    NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (category_id) REFERENCES category (id)
-);
+) ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS reviewer_tag (
     id              BIGINT          NOT NULL    AUTO_INCREMENT,
@@ -47,4 +47,4 @@ CREATE TABLE IF NOT EXISTS reviewer_tag (
     PRIMARY KEY (id),
     FOREIGN KEY (reviewer_id) REFERENCES reviewer (id),
     FOREIGN KEY (tag_id) REFERENCES tag (id)
-);
+) ENGINE = InnoDB;
