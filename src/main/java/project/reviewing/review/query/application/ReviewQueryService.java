2bb247817ea06af0ba364d52bd75dfd6bd6fcce2
ohseil <sioh0612@gmail.com>
2023-04-19 21:13:26 +0900

    feat: Service 로직 구현

--- src/main/java/project/reviewing/review/query/application/ReviewQueryService.java
+++ src/main/java/project/reviewing/review/query/application/ReviewQueryService.java
@@ -1,14 +1,21 @@
 package project.reviewing.review.query.application;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import project.reviewing.review.query.application.response.ReviewsResponse;
+import project.reviewing.review.query.dao.ReviewsDAO;
 
+@RequiredArgsConstructor
 @Transactional(readOnly = true)
 @Service
 public class ReviewQueryService {
 
+    private final ReviewsDAO reviewsDAO;
+
     public ReviewsResponse findReviewsByRole(final Long memberId, final String role) {
-        return new ReviewsResponse();
+        return (role.equals("reviewer")) ?
+                ReviewsResponse.from(reviewsDAO.findReviewsByReviewer(memberId))
+                : ReviewsResponse.from(reviewsDAO.findReviewsByReviewee(memberId));
     }
 }
--- src/main/java/project/reviewing/review/query/application/response/ReviewsResponse.java
+++ src/main/java/project/reviewing/review/query/application/response/ReviewsResponse.java
@@ -1,17 +1,25 @@
 package project.reviewing.review.query.application.response;
 
 import lombok.Getter;
+import project.reviewing.review.query.dao.data.ReviewByRoleData;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 public class ReviewsResponse {
 
     private List<ReviewSummary> reviews;
 
-    public ReviewsResponse() {}
+    public static ReviewsResponse from(List<ReviewByRoleData> reviewByRoleDataList) {
+        return new ReviewsResponse(
+                reviewByRoleDataList.stream()
+                        .map(ReviewSummary::from)
+                        .collect(Collectors.toList())
+        );
+    }
 
-    public ReviewsResponse(final List<ReviewSummary> reviews) {
+    private ReviewsResponse(final List<ReviewSummary> reviews) {
         this.reviews = reviews;
     }
 
@@ -21,7 +29,16 @@ public class ReviewsResponse {
         private final Long reviewerId;
         private final MemberSummary member;
 
-        public ReviewSummary(final Long id, final String title, final Long reviewerId, final MemberSummary member) {
+        private static ReviewSummary from(final ReviewByRoleData reviewByRoleData) {
+            return new ReviewSummary(
+                    reviewByRoleData.getReviewId(), reviewByRoleData.getTitle(), reviewByRoleData.getReviewerId(),
+                    new MemberSummary(
+                            reviewByRoleData.getMemberId(), reviewByRoleData.getUsername(), reviewByRoleData.getImageUrl()
+                    )
+            );
+        }
+
+        private ReviewSummary(final Long id, final String title, final Long reviewerId, final MemberSummary member) {
             this.id = id;
             this.title = title;
             this.reviewerId = reviewerId;
@@ -33,7 +50,7 @@ public class ReviewsResponse {
             private final String username;
             private final String imageUrl;
 
-            public MemberSummary(final Long id, final String username, final String imageUrl) {
+            private MemberSummary(final Long id, final String username, final String imageUrl) {
                 this.id = id;
                 this.username = username;
                 this.imageUrl = imageUrl;
