5920e3f5d22592521b56a7f7de959d9c71b62cec
ohseil <sioh0612@gmail.com>
2023-04-28 21:09:22 +0900

    chore: CORS 관련 프론트 서버 배포 환경 Origin 허용 설정 추가

--- src/main/java/project/reviewing/common/config/WebConfig.java
+++ src/main/java/project/reviewing/common/config/WebConfig.java
@@ -18,7 +18,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000", "http://13.124.78.119")
                 .allowedMethods("GET", "POST", "DELETE", "PUT", "PATCH", "OPTIONS", "HEAD", "TRACE")
                 .allowedHeaders("*")
                 .allowCredentials(true)
