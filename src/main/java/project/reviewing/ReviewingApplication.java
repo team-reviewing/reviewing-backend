b3e4a4bf9bde4e392c34591dbc9402e5953169eb
ohseil <sioh0612@gmail.com>
2023-05-03 17:35:22 +0900

    feat: Scheduler 사용하기 위한 설정 추가

--- src/main/java/project/reviewing/ReviewingApplication.java
+++ src/main/java/project/reviewing/ReviewingApplication.java
@@ -2,7 +2,9 @@ package project.reviewing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class ReviewingApplication {
 
