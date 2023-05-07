61b2fedef7acd8d3cacbf34780ad93ebb3abdd05
ohseil <sioh0612@gmail.com>
2023-05-08 06:14:32 +0900

    feat: 직접 정의한 Exception의 최상위 class에 ErrorType 필드 추가

--- src/main/java/project/reviewing/common/exception/BadRequestException.java
+++ src/main/java/project/reviewing/common/exception/BadRequestException.java
@@ -1,8 +1,13 @@
 package project.reviewing.common.exception;
 
+import lombok.Getter;
+
+@Getter
 public class BadRequestException extends RuntimeException {
 
+    private final ErrorType errorType;
+
     public BadRequestException(final ErrorType errorType) {
-        super(errorType.getMessage());
+        this.errorType = errorType;
     }
 }
--- src/main/java/project/reviewing/common/exception/UnauthorizedException.java
+++ src/main/java/project/reviewing/common/exception/UnauthorizedException.java
@@ -1,8 +1,13 @@
 package project.reviewing.common.exception;
 
+import lombok.Getter;
+
+@Getter
 public class UnauthorizedException extends RuntimeException {
 
+    private final ErrorType errorType;
+
     public UnauthorizedException(final ErrorType errorType) {
-        super(errorType.getMessage());
+        this.errorType = errorType;
     }
 }
