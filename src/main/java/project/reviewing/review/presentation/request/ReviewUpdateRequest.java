cb813f3d10141c1a6c5f8025286da351dbb737c1
ohseil <sioh0612@gmail.com>
2023-05-08 07:02:20 +0900

    chore: ReviewUpdateRequest에서 잘못 된 요청 정보에 대한 message 추가

--- src/main/java/project/reviewing/review/presentation/request/ReviewUpdateRequest.java
+++ src/main/java/project/reviewing/review/presentation/request/ReviewUpdateRequest.java
@@ -12,7 +12,7 @@ import javax.validation.constraints.Size;
 public class ReviewUpdateRequest {
 
     @Size(min = 1, max = 1500, message = "본문은 1500자 이하로 작성해 주세요.")
-    @NotBlank
+    @NotBlank(message = "본문을 입력해 주세요.")
     private String content;
 
     public ReviewUpdateRequest(final String content) {
