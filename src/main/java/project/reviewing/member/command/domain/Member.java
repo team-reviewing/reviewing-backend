41060f642b4954c76fafdd998a747f352122fb33
ohseil <sioh0612@gmail.com>
2023-05-12 02:35:53 +0900

    feat: 리뷰 평가 생성 과정 중에 검증 로직 추가

--- src/main/java/project/reviewing/member/command/domain/Member.java
+++ src/main/java/project/reviewing/member/command/domain/Member.java
@@ -84,4 +84,8 @@ public class Member {
     public void updateReviewerScore(final Float evaluationScore) {
         reviewer.updateScore(evaluationScore);
     }
+
+    public boolean isNotRegisterReviewer() {
+        return reviewer == null;
+    }
 }
