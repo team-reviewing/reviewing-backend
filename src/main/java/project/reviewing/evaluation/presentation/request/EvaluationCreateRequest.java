a3802f0f1f6a9807fa2bc4b5d6a978fda752c85a
ohseil <sioh0612@gmail.com>
2023-05-11 18:19:32 +0900

    feat: Request DTO 데이터 조건 추가

--- src/main/java/project/reviewing/evaluation/presentation/request/EvaluationCreateRequest.java
+++ src/main/java/project/reviewing/evaluation/presentation/request/EvaluationCreateRequest.java
@@ -2,11 +2,20 @@ package project.reviewing.evaluation.presentation.request;
 
 import lombok.Getter;
 
+import javax.validation.constraints.*;
+
 @Getter
 public class EvaluationCreateRequest {
 
+    @NotNull
     private final Long reviewId;
+
+    @Min(0) @Max(5)
+    @NotNull
     private final Float score;
+
+    @Size(min = 1, max = 100, message = "본문은 100자 이하로 작성해 주세요.")
+    @NotBlank(message = "본문을 입력해 주세요.")
     private final String content;
 
     public EvaluationCreateRequest(final Long reviewId, final Float score, final String content) {
