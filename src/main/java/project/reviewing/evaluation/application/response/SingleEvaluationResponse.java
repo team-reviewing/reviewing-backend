57790540023767991cc7dca98868e243846f17e7
ohseil <sioh0612@gmail.com>
2023-05-12 13:58:28 +0900

    feat: SingleEvaluationResponse Class 정의

--- src/main/java/project/reviewing/evaluation/application/response/SingleEvaluationResponse.java
+++ src/main/java/project/reviewing/evaluation/application/response/SingleEvaluationResponse.java
@@ -1,4 +1,22 @@
 package project.reviewing.evaluation.application.response;
 
+import project.reviewing.evaluation.domain.Evaluation;
+
 public class SingleEvaluationResponse {
+
+    private Long id;
+    private Float score;
+    private String content;
+
+    public static SingleEvaluationResponse from(final Evaluation evaluation) {
+        return new SingleEvaluationResponse(evaluation.getId(), evaluation.getScore(), evaluation.getContent());
+    }
+
+    public SingleEvaluationResponse() {}
+
+    private SingleEvaluationResponse(final Long id, final Float score, final String content) {
+        this.id = id;
+        this.score = score;
+        this.content = content;
+    }
 }
