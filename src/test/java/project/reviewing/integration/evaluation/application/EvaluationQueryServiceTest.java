eaf983e10fbc5235a8689caec3fcfcf74d9da88d
ohseil <sioh0612@gmail.com>
2023-05-13 19:55:48 +0900

    test: Service 로직 테스트 코드 추가

--- src/test/java/project/reviewing/integration/evaluation/application/EvaluationQueryServiceTest.java
+++ src/test/java/project/reviewing/integration/evaluation/application/EvaluationQueryServiceTest.java
@@ -4,18 +4,23 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.SliceImpl;
 import project.reviewing.common.exception.ErrorType;
+import project.reviewing.evaluation.command.application.response.EvaluationsForReviewerResponse;
 import project.reviewing.evaluation.command.domain.Evaluation;
 import project.reviewing.evaluation.exception.EvaluationNotFoundException;
 import project.reviewing.evaluation.query.application.EvaluationQueryService;
 import project.reviewing.evaluation.query.application.response.SingleEvaluationResponse;
+import project.reviewing.evaluation.query.dao.data.EvaluationForReviewerData;
 import project.reviewing.integration.IntegrationTest;
 import project.reviewing.member.command.domain.Career;
 import project.reviewing.member.command.domain.Job;
 import project.reviewing.member.command.domain.Member;
 import project.reviewing.member.command.domain.Reviewer;
-import project.reviewing.review.command.domain.Review;
 
+import java.util.List;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -29,7 +34,7 @@ public class EvaluationQueryServiceTest extends IntegrationTest {
 
     @BeforeEach
     void setUp() {
-        evaluationQueryService = new EvaluationQueryService(evaluationRepository);
+        evaluationQueryService = new EvaluationQueryService(evaluationRepository, evaluationsDAO);
     }
 
     @DisplayName("단일 리뷰 평가 조회 시 ")
@@ -45,18 +50,15 @@ public class EvaluationQueryServiceTest extends IntegrationTest {
                     new Member(2L, "bboor", "bboor@gmail.com", "imageUrl", "https://github.com/bboor"),
                     new Reviewer(Job.BACKEND, Career.JUNIOR, Set.of(1L), "소개글")
             );
-            final Review review = createReview(
-                    Review.assign(
-                            reviewee.getId(), reviewerMember.getReviewer().getId(),
-                            "제목", "본문", "prUrl", reviewerMember.getId(), reviewerMember.isReviewer(), time
-                    ));
+            final Long reviewId = 1L;
+
             final Evaluation evaluation = createEvaluation(
                     new Evaluation(
-                            review.getId(), reviewee.getId(), reviewerMember.getReviewer().getId(), 3.5F, "평가 내용"
+                            reviewId, reviewee.getId(), reviewerMember.getReviewer().getId(), 3.5F, "평가 내용"
                     ));
 
             // when, then
-            final SingleEvaluationResponse response = evaluationQueryService.findSingleEvaluationByReviewId(review.getId());
+            final SingleEvaluationResponse response = evaluationQueryService.findSingleEvaluationByReviewId(reviewId);
 
             assertAll(
                     () -> assertThat(response.getId()).isEqualTo(evaluation.getId()),
@@ -75,6 +77,48 @@ public class EvaluationQueryServiceTest extends IntegrationTest {
             assertThatThrownBy(() -> evaluationQueryService.findSingleEvaluationByReviewId(invalidReviewId))
                     .isInstanceOf(EvaluationNotFoundException.class)
                     .hasMessage(ErrorType.EVALUATION_NOT_FOUND.getMessage());
+        }
+    }
+
+    @DisplayName("특정 리뷰어의 리뷰 목록 조회 시 ")
+    @Nested
+    class EvaluationsForReviewerFindTest {
+
+        @DisplayName("정상적으로 조회된다.")
+        @Test
+        void validFindEvaluations() {
+            final Member reviewee1 = createMember(new Member(1L, "Tom", "Tom@gmail.com", "imageUrl", "https://github.com/Tom"));
+            final Member reviewee2 = createMember(new Member(2L, "J", "J@gmail.com", "imageUrl", "https://github.com/J"));
+            final Member reviewerMember = createMemberAndRegisterReviewer(
+                    new Member(3L, "bboor", "bboor@gmail.com", "imageUrl", "https://github.com/bboor"),
+                    new Reviewer(Job.BACKEND, Career.JUNIOR, Set.of(1L), "소개글")
+            );
+            final Pageable pageable = PageRequest.of(0, 2);
+
+            createEvaluation(new Evaluation(1L, reviewee1.getId(), reviewerMember.getReviewer().getId(), 1.5F, "평가1"));
+            createEvaluation(new Evaluation(2L, reviewee2.getId(), reviewerMember.getReviewer().getId(), 2.0F, "평가2"));
+
+            EvaluationsForReviewerResponse expectedResponse = EvaluationsForReviewerResponse.of(
+                    new SliceImpl<>(
+                            List.of(
+                                    new EvaluationForReviewerData(
+                                            1L, reviewee1.getUsername(), reviewee1.getImageUrl(), 1.5F, "평가1"
+                                    ),
+                                    new EvaluationForReviewerData(
+                                            2L, reviewee2.getUsername(), reviewee2.getImageUrl(), 2.0F, "평가2"
+                                    )
+                            ), pageable, false)
+            );
+
+            EvaluationsForReviewerResponse response = evaluationQueryService.findEvaluationsForReviewerInPage(
+                    reviewerMember.getReviewer().getId(), PageRequest.of(0, 2)
+            );
+
+            assertAll(
+                    () -> assertThat(response.getEvaluations()).usingRecursiveFieldByFieldElementComparator()
+                            .containsAll(expectedResponse.getEvaluations()),
+                    () -> assertThat(response.isHasNext()).isEqualTo(expectedResponse.isHasNext())
+            );
         }
     }
 }
