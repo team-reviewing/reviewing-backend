d4a84e28d9d732dc03875e3929a431f18e1d4866
bboor <sioh0612@gmail.com>
2023-08-06 23:53:33 +0900

    [#95] 리뷰 데이터 배치 작업 개선 (#100)
    * chore: 'local' 환경 Profile 설정
    
    * refactor: 만료된 Review Entity 처리 관련 벌크 연산 쿼리 정의
    
    * refactor: 벌크 연산 쿼리를 이용하여 배치 작업 분할하여 처리하도록 개선
    
    * chore: method 이름 변경 내용 적용
    
    * chore: 'dev' 환경에서 SQL Log Level 'trace' -> 'debug'로 변경
    
    * refactor: 기존에 사용하던 Review Domain Method 제거
    
    * refactor: Scheduler 주기 '30초' -> '10분'으로 변경

--- src/main/java/project/reviewing/review/command/domain/ReviewRepository.java
+++ src/main/java/project/reviewing/review/command/domain/ReviewRepository.java
@@ -1,9 +1,11 @@
 package project.reviewing.review.command.domain;
 
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
-import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,4 +16,38 @@ public interface ReviewRepository extends Repository<Review, Long> {
     Optional<Review> findById(Long id);
     Optional<Review> findByRevieweeIdAndReviewerId(Long revieweeId, Long reviewerId);
     void delete(Review entity);
+
+    @Transactional
+    @Modifying
+    @Query(
+            "UPDATE Review r " +
+            "SET r.status = :refused, r.statusSetAt = CURRENT_TIMESTAMP " +
+            "WHERE (:startId < r.id AND r.id <= :endId) " +
+                    "AND (" +
+                        "(r.status = :created AND r.statusSetAt <= :createdExpiredTime) " +
+                        "OR (r.status = :accepted AND r.statusSetAt <= :acceptedExpiredTime) " +
+                    ")"
+    )
+    int updateExpiredReviews(
+            Long startId, Long endId,
+            ReviewStatus refused, ReviewStatus created, ReviewStatus accepted,
+            LocalDateTime createdExpiredTime, LocalDateTime acceptedExpiredTime
+    );
+
+    @Transactional
+    @Modifying
+    @Query(
+            "DELETE FROM Review r " +
+            "WHERE (:startId < r.id AND r.id <= :endId) " +
+                    "AND (" +
+                        "(r.status = :refused AND r.statusSetAt <= :refusedExpiredTime) " +
+                        "OR (r.status = :approved AND r.statusSetAt <= :approvedExpiredTime) " +
+                        "OR (r.status = :evaluated AND r.statusSetAt <= :evaluatedExpiredTime) " +
+                    ")"
+    )
+    int deleteExpiredReviews(
+            Long startId, Long endId,
+            ReviewStatus refused, ReviewStatus approved, ReviewStatus evaluated,
+            LocalDateTime refusedExpiredTime, LocalDateTime approvedExpiredTime, LocalDateTime evaluatedExpiredTime
+    );
 }
--- src/main/java/project/reviewing/review/scheduler/ReviewScheduler.java
+++ src/main/java/project/reviewing/review/scheduler/ReviewScheduler.java
@@ -1,33 +1,73 @@
 package project.reviewing.review.scheduler;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 import project.reviewing.common.util.Time;
+import project.reviewing.member.command.domain.MemberRepository;
 import project.reviewing.review.command.domain.Review;
 import project.reviewing.review.command.domain.ReviewRepository;
+import project.reviewing.review.command.domain.ReviewStatus;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.time.LocalDateTime;
+import java.util.Collections;
 import java.util.List;
 
 @RequiredArgsConstructor
 @Component
 public class ReviewScheduler {
 
+    private static final int BATCH_SIZE = 10000;
+
     private final ReviewRepository reviewRepository;
     private final Time time;
 
-    @Transactional
-    @Scheduled(cron = "${schedule.cron}")
-    public void checkExpirationForAllReview() {
-        final List<Review> reviews = reviewRepository.findAll();
+    @PersistenceContext
+    private final EntityManager em;
 
-        for (Review review : reviews) {
-            if (review.isTimeToChangeToRefusedStatus()) {
-                review.refuse(time);
-            } else if (review.isTimeToRemove()) {
-                reviewRepository.delete(review);
+    @Scheduled(cron = "${schedule.cron}")
+    public void handleExpiredReviews() {
+
+        LocalDateTime createdExpired = time.now().minusDays(ReviewStatus.CREATED.getExpirePeriod());
+        LocalDateTime acceptedExpired = time.now().minusDays(ReviewStatus.ACCEPTED.getExpirePeriod());
+        LocalDateTime refusedExpired = time.now().minusDays(ReviewStatus.REFUSED.getExpirePeriod());
+        LocalDateTime approvedExpired = time.now().minusDays(ReviewStatus.APPROVED.getExpirePeriod());
+        LocalDateTime evaluatedExpired = time.now().minusDays(ReviewStatus.EVALUATED.getExpirePeriod());
+
+        List<Long> list = em.createQuery("SELECT id FROM Review r", Long.class)
+                .getResultList();
+
+        Collections.sort(list);
+
+        long startId = 0;
+        long endId = 0;
+        long maxIdx = (list.size() % BATCH_SIZE == 0) ? 0 : list.size() + BATCH_SIZE;
+
+        for (int i = BATCH_SIZE - 1; i < maxIdx; i += BATCH_SIZE) {
+            if (i < list.size()) {
+                endId = list.get(i);
+            } else {
+                endId = list.get(list.size() - 1);
             }
+
+            reviewRepository.updateExpiredReviews(
+                    startId, endId, ReviewStatus.REFUSED, ReviewStatus.CREATED, ReviewStatus.ACCEPTED,
+                    createdExpired, acceptedExpired
+            );
+            reviewRepository.deleteExpiredReviews(
+                    startId, endId, ReviewStatus.REFUSED, ReviewStatus.APPROVED, ReviewStatus.EVALUATED,
+                    refusedExpired, approvedExpired, evaluatedExpired
+            );
+            startId = endId;
         }
     }
 }
