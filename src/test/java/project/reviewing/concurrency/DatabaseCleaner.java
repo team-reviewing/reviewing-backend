6a854d1b8d5b25b2917e74343ef987bb9391353b
ohseil <sioh0612@gmail.com>
2023-08-31 13:04:57 +0900

    test: 테스트 환경에서 DB 데이터를 전부 삭제시켜 주는 Class 추가

---
+++ src/test/java/project/reviewing/concurrency/DatabaseCleaner.java
@@ -0,0 +1,42 @@
+package project.reviewing.concurrency;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Component;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@ActiveProfiles("test")
+@RequiredArgsConstructor
+@Component
+public class DatabaseCleaner implements InitializingBean {
+
+    @PersistenceContext
+    private final EntityManager entityManager;
+
+    private final List<String> tableNames = new ArrayList<>();
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        List<Object[]> tableInfos = entityManager.createNativeQuery("SHOW TABLES").getResultList();
+        for (Object[] tableInfo : tableInfos) {
+            String tableName = (String) tableInfo[0];
+            tableNames.add(tableName);
+        }
+    }
+
+    @Transactional
+    public void execute() {
+        entityManager.createNativeQuery("SET REFERENTIAL_INTEGRITY FALSE").executeUpdate();
+        for (String tableName : tableNames) {
+            entityManager.createNativeQuery("TRUNCATE TABLE " + tableName).executeUpdate();
+        }
+        entityManager.createNativeQuery("SET REFERENTIAL_INTEGRITY TRUE").executeUpdate();
+    }
+}
